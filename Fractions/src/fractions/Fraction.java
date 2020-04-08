7a033c27ed712c7efa0be168cab509469c716298
Martin Motejlek <martin.motejlek@tul.cz>
2020-04-08 02:48:57 +0200

    Commented testing main

--- Fractions/src/fractions/Fraction.java
+++ Fractions/src/fractions/Fraction.java
@@ -69,26 +69,26 @@ public class Fraction {
                 + Math.abs(nominator) + "/" + Math.abs(denominator);
     }
     
-    public static void main(String[] args) {
-        Fraction a = Fraction.getInstance(-3, 4);
-        //Fraction b = Fraction.getInstance(0, 1);
-        Fraction b = Fraction.getInstance(0, -1);
-        Fraction c = Fraction.getInstance("-54/-72");
-        //Fraction d = Fraction.getInstance("-1/20");
-        Fraction d = Fraction.getInstance("1/-20");
-        
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(c.simplify());
-        System.out.println(d.simplify());
-        System.out.println(a.getNominator());
-        System.out.println(d.getDenominator());
-        System.out.println(Fraction.getInstance(d.toString()).getNominator());
-        
-        //Fraction.getInstance("-20/0");
-        //Fraction.getInstance(4, 0);
-    }
+//    public static void main(String[] args) {
+//        Fraction a = Fraction.getInstance(-3, 4);
+//        //Fraction b = Fraction.getInstance(0, 1);
+//        Fraction b = Fraction.getInstance(0, -1);
+//        Fraction c = Fraction.getInstance("-54/-72");
+//        //Fraction d = Fraction.getInstance("-1/20");
+//        Fraction d = Fraction.getInstance("1/-20");
+//        
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(d);
+//        System.out.println(c.simplify());
+//        System.out.println(d.simplify());
+//        System.out.println(a.getNominator());
+//        System.out.println(d.getDenominator());
+//        System.out.println(Fraction.getInstance(d.toString()).getNominator());
+//        
+//        //Fraction.getInstance("-20/0");
+//        //Fraction.getInstance(4, 0);
+//    }
     
 }
